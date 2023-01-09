02496ecef35e6b30a44c2b7894a5ca43fe2063cd
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-01-09 20:17:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<h1 align="center">Bienvenue sur Jooble 👋</h1>
+<p>
+</p>
+
+> C'est un clone de Google.
+
+
+## Author
+
+👤 **Elias Cardon**
+
+* Github: [@elias-cardon](https://github.com/elias-cardon)
+* LinkedIn: [@Elias Cardon](https://www.linkedin.com/in/elias-cardon-693a31a2/)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Joogle</title>
+</head>
+<body>
+<div class="mainSection">
+    <div class="logoContainer">
+        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Logo">
+    </div>
+
+    <div class="searchContainer">
+        <input type="text" class="searchBox" name="term">
+        <input type="submit" class="searchButton" value="Chercher">
+    </div>
+</div>
+</body>
+</html>
