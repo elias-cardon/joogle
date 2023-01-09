fad067e141ca722e76a1bbfb98e9a389099c5c3f
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-01-09 21:31:59 +0100

    CSS en cours

--- index.php
+++ index.php
@@ -6,18 +6,21 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Joogle</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-<div class="mainSection">
-    <div class="logoContainer">
-        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Logo">
-    </div>
+<div class="wrapper indexPage">
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Logo">
+        </div>
 
-    <div class="searchContainer">
-        <form action="search.php" method="GET">
-            <input type="text" class="searchBox" name="term">
-            <input type="submit" class="searchButton" value="Chercher">
-        </form>
+        <div class="searchContainer">
+            <form action="search.php" method="GET">
+                <input type="text" class="searchBox" name="term">
+                <input type="submit" class="searchButton" value="Chercher">
+            </form>
+        </div>
     </div>
 </div>
 </body>
