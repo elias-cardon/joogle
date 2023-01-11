cf0992cd66ff0c7b4abeba638d23a7492f85f1c1
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-01-11 21:13:05 +0100

    On fait la page de recherche

--- search.php
+++ search.php
@@ -1,3 +1,35 @@
-<?php
-
-echo $_GET["term"];
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Recherche</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="icon" type="image/png" href="./assets/images/Joogle_logo_mini.png" />
+</head>
+<body>
+<div class="wrapper">
+    <div class="header">
+        <div class="headerContent">
+            <div class="logoContainer">
+                <a href="">
+                    <img src="./assets/images/Joogle_logo.png" alt="Logo">
+                </a>
+            </div>
+            <div class="searchContainer">
+                <form action="search.php" method="get">
+                    <div class="searchContainer">
+                        <input type="text" class="searchBox" name="term">
+                        <button>
+                            Chercher
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
