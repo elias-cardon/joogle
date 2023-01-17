c35f07e96473faf66d4c1079c481a86d328c4189
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-01-17 20:21:27 +0100

    Document parser class

---
+++ crawl.php
@@ -0,0 +1,8 @@
+<?php
+include ("./classes/DomDocumentParser.php");
+function followLinks($url) {
+    $parser = new DomDocumentParser($url);
+}
+
+$startUrl = "http://www.joogle.com";
+followLinks($startUrl);
