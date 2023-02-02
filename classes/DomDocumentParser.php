59df6813a7a1ae92faad0a9867614324cea1bfb1
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-02-02 15:31:25 +0100

    Echo les liens des url

--- classes/DomDocumentParser.php
+++ classes/DomDocumentParser.php
@@ -11,4 +11,8 @@ class DomDocumentParser{
         $this->doc = new DomDocument();
         @$this->doc->loadHTML(file_get_contents($url, false, $context));
     }
+
+    public function getlinks(){
+        return $this->doc->getElementsByTagName("a");
+    }
 }
--- crawl.php
+++ crawl.php
@@ -2,7 +2,14 @@
 include ("./classes/DomDocumentParser.php");
 function followLinks($url) {
     $parser = new DomDocumentParser($url);
+
+    $linkList = $parser->getlinks();
+
+    foreach ($linkList as $link) {
+        $href = $link->getAttribute("href");
+        echo $href . "<br>";
+    }
 }
 
-$startUrl = "http://www.joogle.com";
+$startUrl = "http://www.apple.com";
 followLinks($startUrl);
